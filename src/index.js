e81447f9a4ac9b0dae61f65588e2c01f25b379da
neanderdev <neander.faria@hotmail.com>
2022-06-14 10:28:23 -0300

    Validating existing CPF

--- src/index.js
+++ src/index.js
@@ -16,6 +16,14 @@ const customers = [];
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" });
+  }
+
   customers.push({
     id: uuidv4(),
     name,
