1d466ad3175a1599bd0b6ccb7df36faf8bae0880
neanderdev <neander.faria@hotmail.com>
2022-06-14 10:31:50 -0300

    Validating the account

--- src/index.js
+++ src/index.js
@@ -7,12 +7,6 @@ app.use(express.json());
 
 const customers = [];
 
-/**
- * cpf - string
- * name - string
- * id - uuid
- * statement []
- */
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -34,10 +28,14 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement/:cpf", (request, response) => {
-  const { cpf } = request.params;
+app.get("/statement", (request, response) => {
+  const { cpf } = request.headers;
 
   const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
 
   return response.json(customer.statement);
 });
